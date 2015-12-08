8a6dec1e5e26af87354a69085e7ce43aa121b68a
Viktor Rutberg <wishie@gmail.com>
2015-12-08 20:50:54 +0100

    MasterDefaultSplitViewController -> CollapsingSplitViewController

--- BitcoinLog/CollapsingSplitViewController.swift
+++ BitcoinLog/CollapsingSplitViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MasterDefaultSplitViewController: UISplitViewController, UISplitViewControllerDelegate {
+class CollapsingSplitViewController: UISplitViewController, UISplitViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,10 +22,12 @@ class MasterDefaultSplitViewController: UISplitViewController, UISplitViewContro
     func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController:UIViewController, ontoPrimaryViewController primaryViewController:UIViewController) -> Bool {
         guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
         guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
+        
         if topAsDetailController.detailItem == nil {
             // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
             return true
         }
+        
         return false
     }
 }
