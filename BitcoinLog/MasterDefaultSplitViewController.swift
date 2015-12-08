bd7dc7e50f35dcfdf3e9ac45f96d658995c1085c
Viktor Rutberg <wishie@gmail.com>
2015-12-08 20:49:40 +0100

    Collapse view controller if detailItem is nil.

---
+++ BitcoinLog/MasterDefaultSplitViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MasterDefaultSplitViewController.swift
+//  BitcoinLog
+//
+//  Created by Viktor Rutberg on 08/12/15.
+//  Copyright © 2015 Viktor Rutberg. All rights reserved.
+//
+
+import UIKit
+
+class MasterDefaultSplitViewController: UISplitViewController, UISplitViewControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController:UIViewController, ontoPrimaryViewController primaryViewController:UIViewController) -> Bool {
+        guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
+        guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
+        if topAsDetailController.detailItem == nil {
+            // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
+            return true
+        }
+        return false
+    }
+}
