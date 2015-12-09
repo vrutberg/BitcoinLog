fbddef0c3c4181fa5993c3e479db28515e73609d
Viktor Rutberg <wishie@gmail.com>
2015-12-09 22:27:39 +0100

    Added a test on the detail viewmodel.

--- BitcoinLogTests/BitcoinLogTests.swift
+++ BitcoinLogTests/BitcoinLogTests.swift
@@ -7,30 +7,65 @@
 //
 
 import XCTest
+import PromiseKit
+
 @testable import BitcoinLog
+
 
 class BitcoinLogTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testDetailViewModelFormatsDataCorrectly() {
+        let expectation = expectationWithDescription("wat")
+        let vm = DetailTableViewViewModel(ticker: "SNORLAX", api: BitcoinApiMock())
+    
+        vm.fetchData().then({ rate in
+            XCTAssertEqual(vm.ask, "330.49 SNORLAX")
+            XCTAssertEqual(vm.bid, "340.41 SNORLAX")
+            XCTAssertEqual(vm.last, "335.12 SNORLAX")
+            XCTAssertEqual(vm.volume, "1234.56 BTC")
+            XCTAssertEqual(vm.volumePercent, "12.34 %")
+            
+            expectation.fulfill()
+        })
+        
+        waitForExpectationsWithTimeout(100, handler: nil)
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
+    /*
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock {
             // Put the code you want to measure the time of here.
         }
     }
+    */
+}
+
+class BitcoinApiMock: BitcoinApi {
+    func fetchSingleRate(ticker: String) -> Promise<BitcoinRate> {
+        let rate = BitcoinRate(
+            tickerSymbol: ticker,
+            timestamp: "2015-12-09 22:03:44",
+            avg24h: 333.0,
+            ask: 330.49,
+            bid: 340.41,
+            last: 335.12,
+            volume_btc: 1234.56,
+            volume_percent: 12.34
+        )
+        
+        return Promise<BitcoinRate>(rate)
+    }
     
+    func fetchAllRates() -> Promise<BitcoinRateList> {
+        return Promise<BitcoinRateList>(BitcoinRateList(rates: []))
+    }
+    
+    func fetchMultipleRates(ticker: [String]) -> Promise<BitcoinRateList> {
+        return self.fetchAllRates()
+    }
+    
+    func fetchMultipleRates(tickers: String...) -> Promise<BitcoinRateList> {
+        return self.fetchMultipleRates(tickers)
+    }
 }
